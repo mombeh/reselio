9b4910776a5729b07bcb775c2622288e678ab426
mombeh <nadinemombeh@gmail.com>
2026-03-03 10:20:25 +0100

    implemented hash registration logic and JWT login code

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { UsersModule } from './users/users.module';
     }),
     MongooseModule.forRoot(process.env.MONGO_URL as string),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -2,20 +2,41 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './schemas/user.schema';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from './dto/create-user.dto';
+import { ConflictException } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
   constructor(
-    @InjectModel(User.name)
-    private userModel: Model<UserDocument>,
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
   ) {}
 
-  async create(userData: Partial<User>) {
-    const newUser = new this.userModel(userData);
-    return newUser.save();
+
+async create(userData: CreateUserDto) {
+  const existingUser = await this.userModel.findOne({ email: userData.email });
+  if (existingUser) {
+    throw new ConflictException('Email is already registered');
   }
+
+  const hashedPassword = await bcrypt.hash(userData.password, 10);
+  const newUser = new this.userModel({ ...userData, password: hashedPassword });
+  return newUser.save();
+}
 
   async findAll() {
     return this.userModel.find();
+  }
+
+  async findByEmail(email: string) {
+    return this.userModel.findOne({ email });
+  }
+
+  async validateUser(email: string, password: string) {
+    const user = await this.findByEmail(email);
+    if (!user) return null;
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) return null;
+    return user;
   }
 }
