0510dc9a02bcc311d9b9194861b7d88a804a9fb8
mombeh <nadinemombeh@gmail.com>
2026-05-18 12:22:23 +0100

    completed expo setup

--- apps/mobile/hooks/use-color-scheme.ts
+++ apps/mobile/hooks/use-color-scheme.ts
@@ -1,1 +1,5 @@
-export { useColorScheme } from 'react-native';
+import { useColorScheme as useRNColorScheme, type ColorScheme } from 'react-native';
+
+export function useColorScheme(): ColorScheme | null {
+  return useRNColorScheme();
+}
